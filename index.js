5529f66bf93a43edb816bf313f46774bd0b1b642
Taiwo Ayoola <ayoola@glade.ng>
2025-03-15 14:02:04 +0000

    Fixed the login for toggling the faq elements

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 //using the .remove idea
-function toggleAnswer(question) {
-  console.log(question);
-  const allQuestions = document.querySelectorAll(".question");
-  //   const question = question.nextElementSibling;
-  //const question = document.getElementById(id);
-  console.log(question);
-  const arrow = question.querySelector(".arrow");
-  const answer = question.querySelector(".answer");
-  const title = question.querySelector(".question");
+function toggleAnswer(faqElement) {
+  const arrow = faqElement.querySelector(".arrow");
+  const answer = faqElement.querySelector(".answer");
+  const title = faqElement.querySelector(".question");
   const existingActive = document.querySelector(".active");
-  console.log();
-  if (existingActive) {
-    const existingActiveArrow = existingActive.querySelector(".arrow");
-    const existingActiveAnswer = existingActive.querySelector(".answer");
-    const existingActiveTitle = existingActive.querySelector(".question");
-    existingActiveArrow.style.transform = "rotate(0deg)";
-    existingActiveAnswer.style.maxHeight = "0px";
-    existingActiveAnswer.style.opacity = "0";
-    existingActiveTitle.style.color = "hsl(240, 6%, 50%)";
-    allQuestions.forEach((q) => q.classList.remove(".active"));
-  }
 
-  if (arrow.style.transform === "rotate(180deg)") {
-    allQuestions.forEach((q) => q.classList.remove(".active"));
-    question.classList.add("active");
+  // Check if faqElement has class active, if satisfied remove it
+  if (existingActive === faqElement) {
     title.style.color = "hsl(240, 6%, 50%)";
     answer.style.maxHeight = "0px";
     answer.style.opacity = "0";
     arrow.style.transform = "rotate(0deg)";
+    faqElement.classList.remove("active");
   } else {
-    allQuestions.forEach((q) => q.classList.remove(".active"));
-    question.classList.add("active");
+    // if there exist a previously opened faq, remove active class and css properties
+    if (existingActive) {
+      const existingActiveArrow = existingActive?.querySelector(".arrow");
+      const existingActiveAnswer = existingActive?.querySelector(".answer");
+      const existingActiveTitle = existingActive?.querySelector(".question");
+      existingActiveArrow.style.transform = "rotate(0deg)";
+      existingActiveAnswer.style.maxHeight = "0px";
+      existingActiveAnswer.style.opacity = "0";
+      existingActiveTitle.style.color = "hsl(240, 6%, 50%)";
+      existingActive.classList.remove("active");
+    }
+
+    // Add active to incoming and set the css properties
+    faqElement.classList.add("active");
     title.style.color = "black";
     answer.style.maxHeight = answer.scrollHeight + "px";
     answer.style.opacity = "1";
